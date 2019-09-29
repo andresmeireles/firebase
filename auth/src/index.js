8843a2a4b10969a3f22c25a5c282ba2e465e3548
André <andre2meireles@gmail.com>
2019-09-28 22:15:50 -0300

    inital commit

---
+++ auth/src/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+import firebaseui from 'firebaseui'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB0bOtfXHdMkD9QPTb1QP7MTApPqUWQ93s",
+    authDomain: "ninja-voador.firebaseapp.com",
+    databaseURL: "https://ninja-voador.firebaseio.com",
+    projectId: "ninja-voador",
+    storageBucket: "ninja-voador.appspot.com",
+    messagingSenderId: "329654089906",
+    appId: "1:329654089906:web:0833d5409f5d3f45"
+};
+
+firebase.initializeApp(firebaseConfig);
